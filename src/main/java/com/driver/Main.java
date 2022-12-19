0a97c63fabc53a78f6ce4e634abcae5db59ca0ba
ayushdubey7799 <ayushdubey7799@gmail.com>
2022-12-19 12:24:12 +0530

    .

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+         Product p = new Product();
+         p.product(1,2);
+         p.product(1,2,3);
+         p.product(1.0,2.0);
+
+    }
+
+    public static class Product{
+        public int product(int x,int y){
+            return 0;
+        }
+
+        public int product(int x,int y,int z){
+            return 0;
+        }
+
+        public double product(double x,double y){
+            return 0.0;
+        }
+
+
+    }
+
 }
